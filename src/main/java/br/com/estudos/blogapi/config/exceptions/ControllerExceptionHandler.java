ca3f552f47fae21b64c40919f29b54230bfce15c
João Marcelo <joaomarcelo588@gmail.com>
2022-07-19 17:38:32 -0300

    Criado exception de negocio

--- src/main/java/br/com/estudos/blogapi/config/exceptions/ControllerExceptionHandler.java
+++ src/main/java/br/com/estudos/blogapi/config/exceptions/ControllerExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import br.com.estudos.blogapi.handlers.NegocioException;
 import br.com.estudos.blogapi.handlers.RecursoNaoEncontradoException;
 import lombok.RequiredArgsConstructor;
 
@@ -26,6 +27,20 @@ public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
 		var status = HttpStatus.NOT_FOUND;
 		var body = new ResponseError();
 
+		body.setCode(status.value());
+		body.setDescricao(ex.getMessage());
+		return handleExceptionInternal(ex, body, headers, status, request);
+	}
+
+	@ExceptionHandler(NegocioException.class)
+	public ResponseEntity<Object> handleNegocioExceptionException(NegocioException ex, WebRequest request) {
+
+		var headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+
+		var status = HttpStatus.BAD_REQUEST;
+
+		var body = new ResponseError();
 		body.setCode(status.value());
 		body.setDescricao(ex.getMessage());
 		return handleExceptionInternal(ex, body, headers, status, request);
