46c4440685b7e73f10a1a04900eff7b465f99591
João Marcelo <joaomarcelo588@gmail.com>
2022-07-21 12:28:08 -0300

    Adicionado Bean BCrypt

--- src/main/java/br/com/estudos/blogapi/BlogApiApplication.java
+++ src/main/java/br/com/estudos/blogapi/BlogApiApplication.java
@@ -2,6 +2,9 @@ package br.com.estudos.blogapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class BlogApiApplication {
@@ -9,6 +12,12 @@ public class BlogApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApiApplication.class, args);
 
+	}
+
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		return encoder;
 	}
 
 }
