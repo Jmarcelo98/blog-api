568df74629fc6e7ace4f093c406a3a43250395a9
João Marcelo <joaomarcelo588@gmail.com>
2022-07-21 21:14:42 -0300

    Criado query para verificar se o POST é de quem esta logado

--- src/main/java/br/com/estudos/blogapi/repositories/PostRepository.java
+++ src/main/java/br/com/estudos/blogapi/repositories/PostRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.estudos.blogapi.model.entities.Post;
 import br.com.estudos.blogapi.model.entities.User;
@@ -11,5 +12,9 @@ import br.com.estudos.blogapi.model.entities.User;
 public interface PostRepository extends JpaRepository<Post, Integer> {
 
 	List<Post> findAllByUserAndIsPublishedTrueOrderByCreatedAt(User user, Pageable pageRequest);
+
+	@Query(value = "SELECT DISTINCT CASE WHEN EXISTS "
+			+ "(SELECT * FROM POST p WHERE p.id = ?1 and p.id_user = ?2) then 'TRUE' else 'FALSE' end FROM post", nativeQuery = true)
+	Boolean itYourPost(Integer idPost, Integer idLogado);
 
 }
