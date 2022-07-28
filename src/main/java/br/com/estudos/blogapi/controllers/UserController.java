cfdf8f4a0746fe4dd97e43a81f4a9464c1bb20cd
João Marcelo <joaomarcelo588@gmail.com>
2022-07-28 17:29:58 -0300

    Adicionado outras URLs que nao necessitam de acesso

--- src/main/java/br/com/estudos/blogapi/controllers/UserController.java
+++ src/main/java/br/com/estudos/blogapi/controllers/UserController.java
@@ -2,7 +2,9 @@ package br.com.estudos.blogapi.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,6 +35,11 @@ public class UserController {
 	public ResponseEntity<Void> update(@RequestBody UserDTO userDTO) {
 		userService.update(userDTO, jwtUtils.getPrincipal());
 		return ResponseEntity.ok().build();
+	}
+
+	@GetMapping(path = "/{nickname}")
+	public ResponseEntity<UserDTO> findByNickname(@PathVariable("nickname") String nickname) {
+		return ResponseEntity.ok(userService.findByNicknameAndConvertDTO(nickname));
 	}
 
 	@DeleteMapping
