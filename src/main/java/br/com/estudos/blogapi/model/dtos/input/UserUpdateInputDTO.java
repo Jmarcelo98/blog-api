dda66528fde8af93cb820d28dfcf8c65543e0da9
João Marcelo <joaomarcelo588@gmail.com>
2022-08-01 16:50:02 -0300

    Criado input de update

---
+++ src/main/java/br/com/estudos/blogapi/model/dtos/input/UserUpdateInputDTO.java
@@ -0,0 +1,23 @@
+package br.com.estudos.blogapi.model.dtos.input;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserUpdateInputDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private String biography;
+
+	private String urlLinkedin;
+
+	private String urlInstagram;
+
+	private String urlWebSite;
+
+}
