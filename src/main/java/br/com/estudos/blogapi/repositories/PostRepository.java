fac579601a3bc78e157e31cd80e9870f600832a2
João Marcelo <joaomarcelo588@gmail.com>
2022-08-06 01:12:29 -0300

    Apagando query

--- src/main/java/br/com/estudos/blogapi/repositories/PostRepository.java
+++ src/main/java/br/com/estudos/blogapi/repositories/PostRepository.java
@@ -1,7 +1,6 @@
 package br.com.estudos.blogapi.repositories;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,9 +12,6 @@ import br.com.estudos.blogapi.model.entities.User;
 public interface PostRepository extends JpaRepository<Post, Integer> {
 
 	List<Post> findAllByUserAndIsPublishedTrueOrderByPublishedAtDesc(User user, Pageable pageRequest);
-
-//	@Query(value = "SELECT * FROM POST p INNER JOIN Comment m WHERE p.id = m.id_post AND p.ID = ?1 ORDER BY m.created_At DESC", nativeQuery = true)
-//	Optional<Post> findByIdOrdenationComments(Integer id);
 
 	@Query(value = "SELECT DISTINCT CASE WHEN EXISTS "
 			+ "(SELECT * FROM POST p WHERE p.id = ?1 and p.id_user = ?2) then 'TRUE' else 'FALSE' end FROM post", nativeQuery = true)
